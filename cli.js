ff9e23427c5c3c943d74f3b7ebaeea001705647d
李杰 <ajay.li@feisu.com>
2023-03-31 10:24:52 +0800

    first commit

---
+++ cli.js
@@ -0,0 +1,85 @@
+import commandLineArgs from "command-line-args";
+import commandLineUsage from "command-line-usage"
+import { readFile } from "fs/promises";
+import prompts from 'prompts';
+import gitClone from './util/gitClone.js'
+
+//命令行 -v
+const pkg = JSON.parse(
+    await readFile(new URL('./package.json', import.meta.url))
+);
+//配置命令参数
+const optionDefinitions = [
+    { name: 'version', alias: 'v', type: Boolean },
+    { name: 'help', alias: 'h', type: Boolean }
+];
+
+//命令行
+const helpSections = [
+    {
+        header: 'create-fs-cli',
+        content: '一个快速生成模版项目的脚手架',
+    },
+    {
+        header: 'Options',
+        optionList: [
+            {
+                name: 'version',
+                alias: 'v',
+                typeLabel: '{underline boolean}',
+                description: '版本号',
+            },
+            {
+                name: 'help',
+                alias: 'h',
+                typeLabel: '{underline boolean}',
+                description: '帮助',
+            }
+        ],
+    },
+];
+const promptsOptions = [
+    {
+        type: 'text',
+        name: 'name',
+        message: '请输入项目名称'
+    },
+    {
+        type: 'select', //单选
+        name: 'template',
+        message: '请选择一个模板',
+        choices: [
+            { title: 'react+ts', value: 1 },
+            { title: 'vue3+ts', value: 2 }
+        ]
+    }
+];
+//远程项目地址
+const remoteList = {
+    1: 'https://gitee.com/geeksdidi/kittyui.git',
+    2: 'https://github.com/qddidi/easyest.git'
+};
+const getUserInfo = async () => {
+    const res = await prompts(promptsOptions);
+    if (!res.name || !res.template) return;
+    gitClone(`direct:${remoteList[res.template]}`, res.name, { clone: true });
+  };
+const options = commandLineArgs(optionDefinitions);
+const runOptions = () => {
+    if (options.version) {
+        console.log(`v${pkg.version}`);
+        return;
+    }
+    if (options.help) {
+        console.log(commandLineUsage(helpSections));
+        return;
+    }
+    getUserInfo();
+};
+
+runOptions();
+
+
+
+
+
